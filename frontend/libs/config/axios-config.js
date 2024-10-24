84a792c414d6b52a3ecec275f1151a02931f576d
Abidur Rahman Nabil <nabilrt51@gmail.com>
2024-10-24 17:28:19 +0600

    link changed

--- frontend/libs/config/axios-config.js
+++ frontend/libs/config/axios-config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:9000",
+  baseURL: "https://socket-learning.onrender.com/",
 });
 
 instance.interceptors.request.use(
